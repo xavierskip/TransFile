4a9911c70b630b72e35e03612d1b0da76b9618bb
xavier skip <xavierskip@gmail.com>
2013-03-15 00:47:51 +0800

    init

---
+++ MiniHTTPServer.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+""" MiniHTTPServer
+create the a same other wheel
+"""
+__version__ = "0.1"
+
+
+import socket
+import os
+import time
+import string
+
+
+class BaseServer:
+	def __init__(self, server_address):
+		self.host = server_address[0]
+		self.port = server_address[1]
+		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+	def bind(self):
+		self.socket.bind((self.host,self.port))
+		self.socket.listen(5)
+		print 'Server on: http://%s:%d' %(self.host,self.port)
+
+	def server_forever(self):
+		try:
+			while True:
+				conn,addr = self.socket.accept()
+				self.request = conn.recv(2048)
+				response = self.handle_request()
+				print addr[0],'%s %s' %(self.command, self.src)
+				conn.send(response)
+				conn.close()
+		except KeyboardInterrupt, e:
+			self.socket.close()
+			exit()
+
+
+	def handle_request(self):
+		self.RequestHeaders = self.parse_request()
+		self.command        = self.RequestHeaders['method']
+		self.src            = self.RequestHeaders['requestURL']
+		do_command     = 'do_'+ self.command
+		
+		if not hasattr(self,do_command):
+			self.send_error(501, "Unsupported method (%r)" % self.command)
+			return
+
+		method = getattr(self, do_command)
+		response = method() #  do_HEAD() or do_GET() .....
+		return response
+	#method
+	def do_HEAD(self):
+		return self.send_response_head()
+	
+	def do_GET(self):
+		self.do_HEAD()
+		return self.response_head + self.response_body()
+	
+	def do_POST():
+		pass
+	
+	def send_error(self, status_code, message):
+		self.send_response_line(status_code)
+		self.send_header('Content-type','text/html; charset=utf-8')
+		self.end_head()
+		self.content = message
+
+	def send_response_head(self):
+		self.response_head = ''
+		self.path = self.translate_path(self.src) # local path
+		# directory
+		if os.path.isdir(self.path):
+			if self.path.endswith('/'):
+				self.content = self.directory_page()
+				# response head
+				self.send_response_line(200)
+				self.send_header('Content-type','text/html; charset=utf-8')
+				self.send_header('Content-Length',len(self.content))
+				self.send_header('Connection','keep-alive')
+				self.send_header('server',self.server_version)
+				self.send_header('Date',self.GMT_time())
+				self.end_head()
+				return None
+			else:
+				# response head
+				self.send_response_line(301)
+				self.send_header("Location", self.path + "/")
+				self.end_head()
+				self.content = ''
+				return None
+		else:
+			# File
+			self.type = self.guess_type()
+			try:
+				f = open(self.path,'rb')
+				self.content = f.read()
+				f.close()
+			except IOError:
+				self.send_error(404, "<b>%s</b> not found" %self.src)
+				return None
+			# response head
+			self.send_response_line(200)
+			if self.type: 
+				self.send_header('Content-type',self.type)
+			self.send_header('Content-Length',len(self.content))
+			self.send_header('Connection','keep-alive')
+			self.send_header('server',self.server_version)
+			self.send_header('Date',self.GMT_time())
+			#self.send_header('Last_Modified',)
+			self.end_head()
+			return None
+
+	# response_head
+	def send_response_line(self, status_code):
+		self.response_head += '%s %d %s\r\n' %(
+			self.protocol_version, 
+			status_code,
+			self.response_code[status_code][0],)
+
+	def send_header(self, key, value):
+		self.response_head += '%s: %s\r\n' %(key,value)
+
+	def end_head(self):
+		self.response_head += '\r\n'
+
+	def response_body(self):
+		return self.content
+
+
+
+	def parse_request(self):
+		request_parsed = {}
+		#print '%r' % self.request.rstrip('\r\n')
+		request_lines = self.request.rstrip('\r\n').split('\r\n')
+		#print request_lines
+		start_line    = request_lines[0].split(' ',2)
+		request_parsed['method']     = start_line[0]
+		request_parsed['requestURL'] = start_line[1]
+		request_parsed['version']    = start_line[2]
+		for i in request_lines[1:]:
+			header = i.split(':',1)
+			#print header
+			request_parsed[header[0]] = header[1]
+		return request_parsed
+
+	def translate_path(self, path):
+		# relative path
+		path = '.'+path
+		# abandon query parameters
+		path = path.split('?',1)[0]
+		path = path.split('#',1)[0]
+		#path = posixpath.normpath(urllib.unquote(path))  what?
+		#Relative path
+		path = string.replace(path,'..','.')
+		return path
+
+	def directory_page(self):
+		def element(inner,tags):
+			return inner.join(tags)
+		try:
+			list = os.listdir(self.path)
+		except os.error:
+			self.send_error(404,'No permission to list directory')
+		list.sort(key=lambda a: a.lower())
+		li = '<li><a href="..">..<a></li>' #back
+		for i in list:
+			if os.path.isdir(self.path+'/'+i):
+				i+='/'
+			i = '<a href="%(url)s">%(url)s</a>' %{'url':i}
+			li += element(i,['<li>','</li>'])
+		ul = element(li,['<ul>','</ul>'])			
+		return self.Template %{'title':self.src, 'directory':self.src, 'body':ul}
+	
+	def guess_type(self):
+		Extension = self.path.split('.',-1)[-1]
+		try:
+			return self.MIME[Extension]
+		except KeyError:
+			return None
+	
+	# some other  functions
+	def GMT_time(self, timestamp=None):
+		if timestamp is None:
+			timestamp = time.time()
+		year, month, day, hh, mm, ss, wd, y, z = time.gmtime(timestamp)
+		s = "%s, %02d %3s %4d %02d:%02d:%02d GMT" % ( 
+			 self.weekdayname[wd], 
+			 day, self.monthname[month], year,
+			 hh, mm, ss)
+		return s
+
+	def time_now(self):
+		now = time.time()
+		year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
+		s = "%02d/%3s/%04d %02d:%02d:%02d" % (
+			day, self.monthname[month], year, hh, mm, ss)
+		return s
+
+
+	# static class variables
+	server_version = "miniHTTP/" + __version__
+	protocol_version = 'HTTP/1.1'
+	weekdayname = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+	monthname = [None,
+				 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+				 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	MIME = {
+		'py': 'text/plain',
+		'c': 'text/plain',
+		'h': 'text/plain',
+		'css': 'text/css',
+		'jpg': 'image.jpeg',
+		'gif': 'image/gif',
+		'swf': 'application/x-shockwave-flash'
+	}
+	Template = '''
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>%(title)s</title>
+	</head>
+	<body>
+		<h2> %(directory)s</h2>
+		<hr>
+		%(body)s
+	</body>
+	</html>
+	'''
+
+	# Table mapping response codes to messages; entries have the
+	# form {code: (shortmessage, longmessage)}.
+	# See RFC 2616.
+	response_code = {
+		100: ('Continue', 'Request received, please continue'),
+		101: ('Switching Protocols','Switching to new protocol; obey Upgrade header'),
+		200: ('OK', 'Request fulfilled, document follows'),
+		201: ('Created', 'Document created, URL follows'),
+		202: ('Accepted','Request accepted, processing continues off-line'),
+		203: ('Non-Authoritative Information', 'Request fulfilled from cache'),
+		204: ('No Content', 'Request fulfilled, nothing follows'),
+		205: ('Reset Content', 'Clear input form for further input.'),
+		206: ('Partial Content', 'Partial content follows.'),
+
+		300: ('Multiple Choices','Object has several resources -- see URI list'),
+		301: ('Moved Permanently', 'Object moved permanently -- see URI list'),
+		302: ('Found', 'Object moved temporarily -- see URI list'),
+		303: ('See Other', 'Object moved -- see Method and URL list'),
+		304: ('Not Modified','Document has not changed since given time'),
+		305: ('Use Proxy','You must use proxy specified in Location to access this resource.'),
+		307: ('Temporary Redirect','Object moved temporarily -- see URI list'),
+
+		400: ('Bad Request','Bad request syntax or unsupported method'),
+        401: ('Unauthorized','No permission -- see authorization schemes'),
+        402: ('Payment Required','No payment -- see charging schemes'),
+		403: ('Forbidden','Request forbidden -- authorization will not help'),
+		404: ('Not Found', 'Nothing matches the given URI'),
+		405: ('Method Not Allowed','Specified method is invalid for this resource.'),
+		406: ('Not Acceptable', 'URI not available in preferred format.'),
+		407: ('Proxy Authentication Required', 'You must authenticate with this proxy before proceeding.'),
+		408: ('Request Timeout', 'Request timed out; try again later.'),
+		409: ('Conflict', 'Request conflict.'),
+		410: ('Gone','URI no longer exists and has been permanently removed.'),
+		411: ('Length Required', 'Client must specify Content-Length.'),
+		412: ('Precondition Failed', 'Precondition in headers is false.'),
+		413: ('Request Entity Too Large', 'Entity is too large.'),
+		414: ('Request-URI Too Long', 'URI is too long.'),
+		415: ('Unsupported Media Type', 'Entity body in unsupported format.'),
+		416: ('Requested Range Not Satisfiable','Cannot satisfy request range.'),
+		417: ('Expectation Failed','Expect condition could not be satisfied.'),
+
+		500: ('Internal Server Error', 'Server got itself in trouble'),
+		501: ('Not Implemented','Server does not support this operation'),
+		502: ('Bad Gateway', 'Invalid responses from another server/proxy.'),
+		503: ('Service Unavailable','The server cannot process the request due to a high load'),
+		504: ('Gateway Timeout','The gateway server did not receive a timely response'),
+		505: ('HTTP Version Not Supported', 'Cannot fulfill request.'),
+	}
+def test(port=8081):
+	server_address = ('0.0.0.0',port)
+	httpd = BaseServer(server_address)
+	httpd.bind()
+	httpd.server_forever()
+
+if __name__ == '__main__':
+	test()
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 MiniHTTPserver
 ==============
 
-a mini python http server 
+a mini python http server
+
+#重复造轮子 #练手
+
+仿照 simpleHTTPServer 写的个http server.
+
+可以用来在局域网中间分享文件。
+
+实现了 HEAD 和 GET 方法
+
+NEXT：POST 方法，这样就可以开启服务后，对方上传文件。
+
+中文文件链接有问题。
+
+
